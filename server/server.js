413e85197a94ee0b1434ced5890d5bf1ed6a6dbe
stephanie nunez <snunez1231@gmail.com>
2024-03-14 12:38:18 -0400

    test

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
+const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const mongoose = require('mongoose');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+server.applyMiddleware({ app });
+
+mongoose.connect('mongodb://localhost:27017/booksearch', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
+
